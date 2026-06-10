4a37eddbb86b13330c1ea18a8b11e32cf4812a0d
Hossam Soliuman <hossamsoliuman@gmail.com>
2026-06-10 14:54:44 +0300

    feat: apply footer design

--- resources/views/components/public/footer.blade.php
+++ resources/views/components/public/footer.blade.php
@@ -1,9 +1,9 @@
-<div class="ft-dark__main">
+<footer class="ft-dark__main" aria-label="Site Footer">
     <div class="container">
         <div class="row ft-dark__row">
 
-            {{-- Column 1: logo + description + socials --}}
-            <div class="col-xl-3 col-lg-3 col-md-6 ft-dark__col">
+            {{-- Column 1: Brand --}}
+            <div class="col-xl-3 col-lg-4 col-md-12 ft-dark__col ft-dark__brand">
                 <a href="{{ route('home') }}" class="ft-dark__logo">
                     <img src="{{ asset('assets/images/logo/Tectignis-IT-solution-logo.webp') }}"
                          width="160" height="48" class="img-fluid" alt="Tectignis IT Solutions">
@@ -14,22 +14,22 @@
                 </p>
                 <ul class="ft-dark__socials">
                     <li>
-                        <a href="https://in.linkedin.com/company/tectignis" target="_blank" aria-label="LinkedIn">
+                        <a href="https://in.linkedin.com/company/tectignis" target="_blank" rel="noopener" aria-label="Visit LinkedIn">
                             <i class="fab fa-linkedin-in"></i>
                         </a>
                     </li>
                     <li>
-                        <a href="https://x.com/ItTectignis" target="_blank" aria-label="X / Twitter">
+                        <a href="https://x.com/ItTectignis" target="_blank" rel="noopener" aria-label="Visit X / Twitter">
                             <i class="fab fa-twitter"></i>
                         </a>
                     </li>
                     <li>
-                        <a href="https://www.instagram.com/tectignisofficial/" target="_blank" aria-label="Instagram">
+                        <a href="https://www.instagram.com/tectignisofficial/" target="_blank" rel="noopener" aria-label="Visit Instagram">
                             <i class="fab fa-instagram"></i>
                         </a>
                     </li>
                     <li>
-                        <a href="https://www.facebook.com/tectignisofficial/" target="_blank" aria-label="Facebook">
+                        <a href="https://www.facebook.com/tectignisofficial/" target="_blank" rel="noopener" aria-label="Visit Facebook">
                             <i class="fab fa-facebook-f"></i>
                         </a>
                     </li>
@@ -37,73 +37,82 @@
             </div>
 
             {{-- Column 2: Company --}}
-            <div class="col-xl col-lg col-md-3 col-sm-6 ft-dark__col">
+            <nav class="col-xl col-lg-2 col-md-4 col-sm-6 ft-dark__col" aria-label="Company">
                 <h6 class="ft-dark__heading">Company</h6>
                 <ul class="ft-dark__links">
+                    <li><a href="{{ route('about') }}">About Us</a></li>
                     <li><a href="{{ route('about') }}">Why Tectignis</a></li>
-                    <li><a href="{{ route('services.index') }}">Our Services</a></li>
-                    <li><a href="{{ route('case-studies.index') }}">Case Studies</a></li>
-                    <li><a href="{{ route('blog.index') }}">Blog</a></li>
                     <li><a href="{{ route('careers') }}">Careers</a></li>
+                    <li><a href="{{ route('technology-insights') }}">News &amp; Insights</a></li>
                     <li><a href="{{ route('contact') }}">Contact Us</a></li>
                 </ul>
-            </div>
+            </nav>
 
             {{-- Column 3: Capabilities --}}
-            <div class="col-xl col-lg col-md-3 col-sm-6 ft-dark__col">
+            <nav class="col-xl col-lg-2 col-md-4 col-sm-6 ft-dark__col" aria-label="Capabilities">
                 <h6 class="ft-dark__heading">Capabilities</h6>
                 <ul class="ft-dark__links">
                     <li><a href="{{ route('capabilities.index') }}">Software Development</a></li>
-                    <li><a href="{{ route('capabilities.index') }}">Mobile App Development</a></li>
                     <li><a href="{{ route('capabilities.index') }}">AI &amp; Automation</a></li>
                     <li><a href="{{ route('capabilities.index') }}">Cloud &amp; Infrastructure</a></li>
+                    <li><a href="{{ route('capabilities.index') }}">Data &amp; Analytics</a></li>
                     <li><a href="{{ route('capabilities.index') }}">Cybersecurity</a></li>
                     <li><a href="{{ route('capabilities.index') }}">Smart Security</a></li>
-                    <li><a href="{{ route('capabilities.index') }}">Digital Marketing</a></li>
                 </ul>
-            </div>
+            </nav>
 
-            {{-- Column 4: Solutions --}}
-            <div class="col-xl col-lg col-md-3 col-sm-6 ft-dark__col">
-                <h6 class="ft-dark__heading">Solutions</h6>
-                <ul class="ft-dark__links">
-                    <li><a href="{{ route('services.index') }}">ERP Solutions</a></li>
-                    <li><a href="{{ route('services.index') }}">CRM Solutions</a></li>
-                    <li><a href="{{ route('services.index') }}">HRMS Solutions</a></li>
-                    <li><a href="{{ route('services.index') }}">AI Solutions</a></li>
-                    <li><a href="{{ route('services.index') }}">Automation Solutions</a></li>
-                    <li><a href="{{ route('services.index') }}">Smart Security Solutions</a></li>
-                    <li><a href="{{ route('services.index') }}">Digital Marketing</a></li>
-                </ul>
-            </div>
-
-            {{-- Column 5: Industries --}}
-            <div class="col-xl col-lg col-md-3 col-sm-6 ft-dark__col">
+            {{-- Column 4: Industries --}}
+            <nav class="col-xl col-lg-2 col-md-4 col-sm-6 ft-dark__col" aria-label="Industries">
                 <h6 class="ft-dark__heading">Industries</h6>
                 <ul class="ft-dark__links">
                     <li><a href="{{ route('industries.index') }}">Manufacturing</a></li>
-                    <li><a href="{{ route('industries.index') }}">Retail &amp; eCommerce</a></li>
-                    <li><a href="{{ route('industries.index') }}">BFSI &amp; NBFCs</a></li>
                     <li><a href="{{ route('industries.index') }}">Healthcare</a></li>
+                    <li><a href="{{ route('industries.index') }}">Retail &amp; eCommerce</a></li>
+                    <li><a href="{{ route('industries.index') }}">Logistics</a></li>
                     <li><a href="{{ route('industries.index') }}">Finance &amp; Banking</a></li>
-                    <li><a href="{{ route('industries.index') }}">Data &amp; Analytics</a></li>
+                    <li><a href="{{ route('industries.index') }}">Education</a></li>
                 </ul>
-            </div>
+            </nav>
 
-            {{-- Column 6: Quick Links --}}
-            <div class="col-xl col-lg col-md-3 col-sm-6 ft-dark__col">
-                <h6 class="ft-dark__heading">Quick Links</h6>
+            {{-- Column 5: Resources --}}
+            <nav class="col-xl col-lg-3 col-md-6 col-sm-6 ft-dark__col" aria-label="Resources">
+                <h6 class="ft-dark__heading">Resources</h6>
                 <ul class="ft-dark__links">
-                    <li><a href="{{ route('sitemap') }}">Sitemap</a></li>
+                    <li><a href="{{ route('case-studies.index') }}">Case Studies</a></li>
+                    <li><a href="{{ route('blog.index') }}">Blog</a></li>
                     <li><a href="{{ route('technology-insights') }}">Technology Insights</a></li>
                     <li><a href="{{ route('downloads') }}">Downloads</a></li>
                     <li><a href="{{ route('faqs') }}">FAQs</a></li>
+                    <li><a href="{{ route('sitemap') }}">Sitemap</a></li>
+                </ul>
+            </nav>
+
+            {{-- Column 6: Contact --}}
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 ft-dark__col">
+                <h6 class="ft-dark__heading">Contact Us</h6>
+                <ul class="ft-dark__contact">
+                    <li>
+                        <i class="fas fa-map-marker-alt"></i>
+                        <span>Navi Mumbai,<br>Maharashtra, India</span>
+                    </li>
+                    <li>
+                        <i class="fas fa-phone-alt"></i>
+                        <a href="tel:+919987705688">+91 9987705688</a>
+                    </li>
+                    <li>
+                        <i class="fas fa-envelope"></i>
+                        <a href="mailto:info@tectignis.in">info@tectignis.in</a>
+                    </li>
+                    <li>
+                        <i class="fas fa-clock"></i>
+                        <span>Mon - Sat: 9AM - 8PM</span>
+                    </li>
                 </ul>
             </div>
 
         </div>
     </div>
-</div>
+</footer>
 
 <div class="ft-dark__copy">
     <div class="container">
@@ -113,11 +122,11 @@
                 <a href="{{ route('home') }}">Tectignis IT Solutions Pvt. Ltd.</a>
                 All Rights Reserved.
             </span>
-            <span class="ft-dark__copy-reg">
-                GST: 27AAHFT31725F1Z3
-            </span>
-            <span class="ft-dark__copy-reg">
-                CIN: U72900MH2023PTC401102
+            <span class="ft-dark__copy-reg">GSTIN: 27AAHFT31725F1Z3</span>
+            <span class="ft-dark__copy-reg">CIN: U72900MH2023PTC401102</span>
+            <span class="ft-dark__copy-legal">
+                <a href="{{ route('legal.show', 'privacy-policy') }}">Privacy Policy</a>
+                <a href="{{ route('legal.show', 'terms-and-conditions') }}">Terms of Service</a>
             </span>
         </div>
     </div>
